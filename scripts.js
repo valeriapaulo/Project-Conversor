f2b6b9952260a402f976a1ccd0598d5ed3917cff
Valeria De Paulo <valeriapaulo10@gmail.com>
2022-08-08 14:53:23 -0300

    add javascript and API for read the actual conversion of money

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 const button = document.getElementById("convert-button")
 const select = document.getElementById("currency-select")
 
-const dolar = 5.2
-const euro = 5.9
-const bitcoin = 98.715
 
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById("input-real").value
     const realValueText = document.getElementById("real-value-text")
     const currencyValueText = document.getElementById("currency-value-text")
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+    
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+     
 
     // Maneira antiga --> 
     realValueText.innerHTML = inputReais
@@ -37,7 +41,8 @@ const convertValues = () => {
     if (select.value === "Bitcoin") {
         currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
             style: "decimal",
-            
+            currency: "BTC"
+
         }).format(inputReais / bitcoin)
     }
 }
